25cec7531559b0e6b92635fd0e37ba22969d5bc1
LMQ <2798712275@example.com>
2021-04-30 16:13:54 +0800

    Springboot：上传文件大小超出限制如何捕获异常3

--- src/main/java/com/example/common/handle/ApplicationAdviceHandle.java
+++ src/main/java/com/example/common/handle/ApplicationAdviceHandle.java
@@ -2,11 +2,14 @@ package com.example.common.handle;
 
 
 import com.example.common.constanct.CodeType;
+import com.example.common.data.Result;
 import com.example.common.exception.ApplicationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
